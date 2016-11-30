2a47e7c3886978bc1c80d401a9c61e655a61b5a5
Abhishek Singh <abhsing@adobe.com>
2016-11-30 22:57:00 +0530

    adding new files

---
+++ test/second.js
@@ -0,0 +1,16 @@
+
+describe("feature 2 test",function(){
+
+    beforeEach(function(){
+        console.log("inside beforeEach");
+    });
+
+    it("demo test",function(){
+        console.log("inside test");
+    });
+
+    afterEach(function(){
+        console.log("afterEach");
+    });
+
+});
